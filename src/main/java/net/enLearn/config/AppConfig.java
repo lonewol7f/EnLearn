84dbdbcdb1e97c009f7d816b2f26c839ce34b5d4
Kalana <kalana.madusanka26765@gmail.com>
2021-07-14 22:57:31 +0530

    Update AppConfig.java
    add support for resources directory

--- src/main/java/net/enLearn/config/AppConfig.java
+++ src/main/java/net/enLearn/config/AppConfig.java
@@ -13,6 +13,8 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
@@ -28,7 +30,7 @@ import java.util.Properties;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "net.enLearn")
 @PropertySource("classpath:enLearn.properties")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     // setup a variable to hold property
     @Autowired
@@ -43,6 +45,11 @@ public class AppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
     @Bean
