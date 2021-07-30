1215c5ee0ab91fe00e20a29b9ae230f89777d1f7
Kalana <kalana.madusanka26765@gmail.com>
2021-07-30 21:16:23 +0530

    Create RecordedVideoService.java

---
+++ src/main/java/net/enLearn/service/RecordedVideoService.java
@@ -0,0 +1,13 @@
+package net.enLearn.service;
+
+import net.enLearn.entity.RecordedVideo;
+
+/**
+ * Created by Kalana on 30/07/2021
+ */
+
+public interface RecordedVideoService {
+
+    RecordedVideo getVideoById(int id);
+
+}
