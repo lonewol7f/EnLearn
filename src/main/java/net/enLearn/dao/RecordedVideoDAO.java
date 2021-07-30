a6677bfbccbc03140a0fe57fbb59924228212d1f
Kalana <kalana.madusanka26765@gmail.com>
2021-07-30 21:15:08 +0530

    Create RecordedVideoDAO.java

---
+++ src/main/java/net/enLearn/dao/RecordedVideoDAO.java
@@ -0,0 +1,13 @@
+package net.enLearn.dao;
+
+import net.enLearn.entity.RecordedVideo;
+
+/**
+ * Created by Kalana on 30/07/2021
+ */
+
+public interface RecordedVideoDAO {
+
+    RecordedVideo getVideoById(int id);
+
+}
