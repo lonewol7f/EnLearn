7f58094b5fabf1420a53fa290c0dac6ae58dec7a
Kalana <kalana.madusanka26765@gmail.com>
2021-07-30 21:16:17 +0530

    Update CommentServiceImpl.java

--- src/main/java/net/enLearn/service/CommentServiceImpl.java
+++ src/main/java/net/enLearn/service/CommentServiceImpl.java
@@ -1,8 +1,12 @@
 package net.enLearn.service;
 
 import net.enLearn.dao.CommentDAO;
+import net.enLearn.entity.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by Kalana on 29/07/2021
@@ -14,4 +18,26 @@ public class CommentServiceImpl implements CommentService{
     @Autowired
     private CommentDAO commentDAO;
 
+    @Override
+    @Transactional
+    public List<Comment> getCommentsByVideoId(int id) {
+        return commentDAO.getCommentsByVideoId(id);
+    }
+
+    @Override
+    @Transactional
+    public void saveOrUpdateComment(Comment comment) {
+        commentDAO.saveOrUpdateComment(comment);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCommentByCommentId(int id) {
+        commentDAO.deleteCommentByCommentId(id);
+    }
+
+    @Override
+    public Comment getCommentByCommentId(int id) {
+        return commentDAO.getCommentByCommentId(id);
+    }
 }
