9a7f13c6383d5130f01741dcac847744f01ab06a
Kalana <kalana.madusanka26765@gmail.com>
2021-07-30 21:16:41 +0530

    Update ReplyServiceImpl.java

--- src/main/java/net/enLearn/service/ReplyServiceImpl.java
+++ src/main/java/net/enLearn/service/ReplyServiceImpl.java
@@ -1,8 +1,12 @@
 package net.enLearn.service;
 
 import net.enLearn.dao.ReplyDAO;
+import net.enLearn.entity.Reply;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by Kalana on 29/07/2021
@@ -14,4 +18,27 @@ public class ReplyServiceImpl implements ReplyService{
     @Autowired
     private ReplyDAO replyDAO;
 
+    @Override
+    @Transactional
+    public List<Reply> getRepliesByCommentId(int id) {
+        return replyDAO.getRepliesByCommentId(id);
+    }
+
+    @Override
+    @Transactional
+    public void saveOrUpdateReply(Reply reply) {
+        replyDAO.saveOrUpdateReply(reply);
+    }
+
+    @Override
+    @Transactional
+    public void deleteReplyByReplyId(int id) {
+        replyDAO.deleteReplyByReplyId(id);
+    }
+
+    @Override
+    @Transactional
+    public Reply getReplyByReplyId(int id) {
+        return replyDAO.getReplyByReplyId(id);
+    }
 }
