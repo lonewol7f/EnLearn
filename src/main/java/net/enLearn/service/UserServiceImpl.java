923fa4d8edef47e7c5ac864650ba4b963175ed7e
Kalana <kalana.madusanka26765@gmail.com>
2021-07-30 21:16:57 +0530

    Create UserServiceImpl.java

---
+++ src/main/java/net/enLearn/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package net.enLearn.service;
+
+import net.enLearn.dao.UserDAO;
+import net.enLearn.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Kalana on 30/07/2021
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    @Transactional
+    public User getUserById(int id) {
+        return userDAO.getUserById(id);
+    }
+}
