4d16be7257bd0263615bd915b252d8c0c4e26bc4
Kalana <kalana.madusanka26765@gmail.com>
2021-08-22 09:44:35 +0530

    Update Teacher.java

--- src/main/java/net/enLearn/entity/Teacher.java
+++ src/main/java/net/enLearn/entity/Teacher.java
@@ -1,8 +1,10 @@
 package net.enLearn.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by Kalana on 05/08/2021
@@ -12,4 +14,19 @@ import javax.persistence.Table;
 @Table(name = "teacher")
 @PrimaryKeyJoinColumn(name = "id")
 public class Teacher extends User{
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "teacher", cascade = CascadeType.ALL)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<Notification> notifications;
+
+    public Teacher() {
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
 }
