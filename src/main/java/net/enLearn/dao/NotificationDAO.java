3ae90090def15a9420137ca7887e5f7f8898fb86
Kalana <kalana.madusanka26765@gmail.com>
2021-09-04 23:25:57 +0530

    Create NotificationDAO.java

---
+++ src/main/java/net/enLearn/dao/NotificationDAO.java
@@ -0,0 +1,12 @@
+package net.enLearn.dao;
+
+import net.enLearn.entity.Notification;
+
+/**
+ * Created by Kalana on 04/09/2021
+ */
+
+public interface NotificationDAO {
+
+    void addNotification(Notification notification);
+}
