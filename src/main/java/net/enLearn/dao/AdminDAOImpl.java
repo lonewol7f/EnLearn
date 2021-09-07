08b047b39877273771b4c42e185a7c66dc262c77
Kalana <kalana.madusanka26765@gmail.com>
2021-09-07 22:44:29 +0530

    Create AdminDAOImpl.java

---
+++ src/main/java/net/enLearn/dao/AdminDAOImpl.java
@@ -0,0 +1,27 @@
+package net.enLearn.dao;
+
+import net.enLearn.entity.Admin;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Kalana on 07/09/2021
+ */
+
+@Repository
+public class AdminDAOImpl implements AdminDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Admin getAdminById(int adminId) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Admin admin = session.get(Admin.class, adminId);
+
+        return admin;
+    }
+}
