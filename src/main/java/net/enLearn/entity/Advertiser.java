2a3e08a330ab399fba8a2a8770fa023ee6fbef43
thilinianjalika <brittothilini@gmail.com>
2021-09-09 23:13:53 +0530

    Advertisement confirmation page

--- src/main/java/net/enLearn/entity/Advertiser.java
+++ src/main/java/net/enLearn/entity/Advertiser.java
@@ -1,8 +1,7 @@
 package net.enLearn.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 /**
  * Created by Kalana on 05/08/2021
@@ -11,5 +10,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "advertiser")
 @PrimaryKeyJoinColumn(name = "id")
-public class Advertiser extends User{
+
+public class Advertiser extends User {
 }
