85252173a0ebd09a2d2a002cf21e6df1c424b2c1
Kalana <kalana.madusanka26765@gmail.com>
2021-09-09 22:33:24 +0530

    Create SecurityWebApplicationInitializer.java

---
+++ src/main/java/net/enLearn/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package net.enLearn.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Kalana on 09/09/2021
+ */
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
