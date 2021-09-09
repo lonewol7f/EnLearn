8b40e69897be38d7d40f32b7a5c19067dd040d2d
Kalana <kalana.madusanka26765@gmail.com>
2021-09-09 22:33:53 +0530

    Update UserDAOImpl.java

--- src/main/java/net/enLearn/dao/UserDAOImpl.java
+++ src/main/java/net/enLearn/dao/UserDAOImpl.java
@@ -3,7 +3,11 @@ package net.enLearn.dao;
 import net.enLearn.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -23,5 +27,26 @@ public class UserDAOImpl implements UserDAO{
         User user = session.get(User.class, id);
 
         return user;
+    }
+
+    @Override
+    public int getLoggedUserId() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String userName = authentication.getName();
+
+            Query<User> query = session.createQuery("from User where email=:email", User.class);
+            query.setParameter("email", userName);
+
+            User user = query.getSingleResult();
+            int userId = user.getId();
+
+            return userId;
+        }
+
+        return 0;
     }
 }
