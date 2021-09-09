ac36e22c7eb337d90b9d48e0aa1402304464bb55
Kalana <kalana.madusanka26765@gmail.com>
2021-09-09 23:22:56 +0530

    Update SecurityConfig.java

--- src/main/java/net/enLearn/config/SecurityConfig.java
+++ src/main/java/net/enLearn/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasAnyRole("ADMIN", "TEACHER", "STUDENT")
+                .antMatchers("/").permitAll()
                 .antMatchers("/admins/**").hasRole("ADMIN")
                 .antMatchers("/teachers/**").hasRole("TEACHER")
                 .and()
