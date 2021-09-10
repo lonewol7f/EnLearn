bf8d10237dd0353c9afaf62f6485eb5831cf8911
Osini Kithma <66475844+swee76@users.noreply.github.com>
2021-09-10 10:33:51 +0530

    Few changes on Free quiz DAO, service files butt all are commented

--- src/main/java/net/enLearn/service/FreeQuizService.java
+++ src/main/java/net/enLearn/service/FreeQuizService.java
@@ -1,16 +1,15 @@
 package net.enLearn.service;
 
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
-import net.enLearn.entity.FreeQuiz;
-
 /**
  * Created by Osini Kithma
  * 8/26/21
  */
 public interface FreeQuizService {
-
+/*
     List<FreeQuiz> getFreeQuizListById(int id);
     void saveOrUpdateFreeQuiz(FreeQuiz freeQuiz);
     void deleteFreeQuiz(int FreeQuizId);
-    List<FreeQuiz > getFreeQuizByGrade(String greade)
+    List<FreeQuiz> getFreeQuizByGrade(String grade);
+
+ */
 }
