9d21391044964989ad898defde4f8687cf34e69c
Kalana <kalana.madusanka26765@gmail.com>
2021-09-10 10:52:37 +0530

    Update AuthController.java

--- src/main/java/net/enLearn/controller/AuthController.java
+++ src/main/java/net/enLearn/controller/AuthController.java
@@ -1,6 +1,8 @@
 package net.enLearn.controller;
 
+import net.enLearn.entity.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,23 +15,25 @@ import javax.servlet.http.HttpServletRequest;
 public class AuthController {
 
     @GetMapping("/login")
-    public String showLoginPage() {
+    public String showLoginPage(Model model) {
+
+        model.addAttribute("user", new Student());
+
         return "login";
     }
 
     @GetMapping("/profile")
     public String showProfile(HttpServletRequest request) {
-
-        if (request.isUserInRole("ROLE_STUDENT")) {
-            return "redirect:/students";
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "redirect:/admins";
         }
         if (request.isUserInRole("ROLE_TEACHER")) {
             return "redirect:/teachers";
         }
-        if (request.isUserInRole("ROLE_ADMIN")) {
-            return "redirect:/admins";
+        if (request.isUserInRole("ROLE_STUDENT")) {
+            return "redirect:/students";
         }
-
+        
         return "redirect:/login";
     }
 }
