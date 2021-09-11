67c3e83ef3f1c65b6c0bd69e334eae123d0c3f59
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 23:49:47 +0530

    Create TeacherServiceImpl.java

---
+++ src/main/java/net/enLearn/service/TeacherServiceImpl.java
@@ -0,0 +1,22 @@
+package net.enLearn.service;
+
+import net.enLearn.dao.TeacherDAO;
+import net.enLearn.entity.Teacher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TeacherServiceImpl implements TeacherService{
+
+    @Autowired
+    private TeacherDAO teacherDAO;
+
+    @Override
+    @Transactional
+    public Teacher getTeacherById(int id) {
+        return teacherDAO.getTeacherById(id);
+    }
+
+
+}
