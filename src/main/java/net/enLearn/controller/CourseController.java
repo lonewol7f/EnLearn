a27516f6072ce052d91345fa36a3663258a919c8
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 23:48:04 +0530

    Update CourseController.java

--- src/main/java/net/enLearn/controller/CourseController.java
+++ src/main/java/net/enLearn/controller/CourseController.java
@@ -1,33 +1,41 @@
 
 
 package net.enLearn.controller;
+
 import net.enLearn.entity.Course;
 import net.enLearn.service.CourseService;
+import net.enLearn.service.TeacherService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Kalana on 20/07/2021
  */
+
 @Controller
 @RequestMapping("/courses")
 public class CourseController {
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private TeacherService teacherService;
+
     @Autowired
     private Logger logger;
+
     @GetMapping("")
-    public String showCoursePage(){
+    public String showCoursePage() {
         return "course-page";
     }
 
     @GetMapping("/create-courses")
-    public String showCreateCoursePage(){
+    public String showCreateCoursePage(Model model) {
+        Course course = new Course();
+        model.addAttribute("course", course);
         return "create-course";
     }
 
@@ -61,13 +69,43 @@ public class CourseController {
         return "zoom-create";
     }
 
+    //    @PostMapping("/save")
+    //    public String saveCourse(@ModelAttribute("course") Course course, @RequestParam("teacherId") int teacherId, RedirectAttributes redirectAttributes) {
+    //        Teacher teacher = teacherService.getTeacherById(teacherId);
+    //        course.setTeacher(teacher);
+    //        courseService.saveOrUpdate(course);
+    //        redirectAttributes.addAttribute("teacherId", teacherId);
+    //        return "redirect:/teachers";
+    //    }
+
     @PostMapping("/save")
     public String saveCourse(@ModelAttribute("course") Course course) {
         courseService.saveOrUpdate(course);
-        return "profile-page-teacher";
+        return "redirect:/teachers";
     }
 
 
+    @GetMapping("/update")
+    public String showCourseUpdateForm(@RequestParam("courseId") int id, Model model) {
+        Course course = courseService.getCourseById(id);
+        model.addAttribute("course", course);
+        return "create-course";
+    }
+
+    //    @GetMapping("/delete")
+    //    public String deleteCourse(@RequestParam("courseId") int id, Model model, RedirectAttributes redirectAttribute) {
+    //        Course course = courseService.getCourseById(id);
+    //        Teacher teacher = course.getTeacher();
+    //        redirectAttribute.addAttribute("teacherId", teacher.getId());
+    //        courseService.deleteCourse(id);
+    //        return "redirect:/teachers";
+    //    }
+
+    @GetMapping("/delete")
+    public String deleteTeacher(@RequestParam("courseId") int id) {
+        courseService.deleteCourse(id);
+        return "redirect:/teachers";
+    }
 
 }
 
