54ee45ae09e06eb3fedbbb6b8efb97015bf5be15
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 23:14:46 +0530

    Update TeacherController.java

--- src/main/java/net/enLearn/controller/TeacherController.java
+++ src/main/java/net/enLearn/controller/TeacherController.java
@@ -1,11 +1,18 @@
 
 
 package net.enLearn.controller;
+
+import net.enLearn.entity.Event;
+import net.enLearn.service.EventService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 /**
  * Created by Kalana on 21/07/2021
  */
@@ -14,24 +21,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TeacherController {
     @Autowired
     private Logger logger;
+
+    @Autowired
+    private EventService eventService;
+
     @GetMapping("")
     public String showTeacherProfilePage() {
         return "profile-page-teacher";
     }
+
     @GetMapping("/register")
     public String showTeacherRegisterPage() {
         return "register-teacher";
     }
-    
+
     @GetMapping("/Teacher-confirm")
     public String showTeacherConfirm() {
         return "Teacher-confirm";
     }
-    
+
     @GetMapping("/notifications")
-    public String showNotificationPage() {
+    public String showNotificationPage(Model model) {
+        List<Event> events = eventService.getEventList();
+        model.addAttribute("events", events);
+
         return "notification-view";
     }
+
     @GetMapping("/marks-and-access")
     public String showMarksAndAccessPage() {
         return "marks-and-access";
