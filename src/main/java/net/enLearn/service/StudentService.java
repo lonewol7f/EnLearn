5a4b27656546875932b7cee83f2125cca0fd544f
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 14:15:02 +0530

    Create StudentService.java

---
+++ src/main/java/net/enLearn/service/StudentService.java
@@ -0,0 +1,13 @@
+package net.enLearn.service;
+
+import net.enLearn.entity.Student;
+
+/**
+ * Created by Kalana on 10/09/2021
+ */
+
+public interface StudentService {
+
+    void registerUser(Student student);
+
+}
