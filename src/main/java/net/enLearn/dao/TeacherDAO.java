eed21c4613468cf9ffd4b90a63e1c12313d604d2
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 23:48:53 +0530

    Create TeacherDAO.java

---
+++ src/main/java/net/enLearn/dao/TeacherDAO.java
@@ -0,0 +1,13 @@
+package net.enLearn.dao;
+
+import net.enLearn.entity.Teacher;
+
+/**
+ * Created by Kalana on 11/09/2021
+ */
+
+public interface TeacherDAO {
+
+    Teacher getTeacherById(int id);
+
+}
