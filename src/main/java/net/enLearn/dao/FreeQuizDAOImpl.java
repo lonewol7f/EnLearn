6c31b121e335ed8ba11e56ef09725ea1b88eab51
Kalana <kalana.madusanka26765@gmail.com>
2021-09-11 23:48:46 +0530

    Update FreeQuizDAOImpl.java

--- src/main/java/net/enLearn/dao/FreeQuizDAOImpl.java
+++ src/main/java/net/enLearn/dao/FreeQuizDAOImpl.java
@@ -1,24 +1,32 @@
 package net.enLearn.dao;
 
+import net.enLearn.entity.FreeQuiz;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Created by Osini Kithma
  * 8/25/21
  */
 
-//@Repository
-public class FreeQuizDAOImpl /*<Grade> implements FreeQuizDAO */{
+@Repository
+public class FreeQuizDAOImpl implements FreeQuizDAO{
 
-/*    @Autowired
+    @Autowired
     private SessionFactory sessionFactory;
 
     @Override
-    public List<FreeQuiz> getFreeQuizListById(int id){
+    public List<FreeQuiz> getFreeQuizList(){
         // get current session
         Session session = sessionFactory.getCurrentSession();
 
-        //get Quiz associated with grade
-        Grade grade = new Grade();
-        List<FreeQuiz> freeQuizList = grade.getFreeQuizzes();
+        Query<FreeQuiz> freeQuizQuery = session.createQuery("from FreeQuiz");
+        List<FreeQuiz> freeQuizList = freeQuizQuery.getResultList();
 
         return freeQuizList;
 
@@ -34,14 +42,39 @@ public class FreeQuizDAOImpl /*<Grade> implements FreeQuizDAO */{
     }
 
     @Override
-    public void deleteFreeQuiz(int FreeQuizId){
+    public void deleteFreeQuiz(int id){
         // get current session
         Session session = sessionFactory.getCurrentSession();
 
-        String freeQuiz = new String();
+        //get free quiz link
+        FreeQuiz freeQuiz = getFreeQuizByQuizId(id);
+        freeQuiz.getTeacher().getFreeQuizList().remove(freeQuiz);
+
         session.delete(freeQuiz);
     }
 
- */
+    @Override
+    public FreeQuiz getFreeQuizByQuizId(int id){
+        // get current session
+        Session session = sessionFactory.getCurrentSession();
+
+        //get free quiz
+        FreeQuiz freeQuiz = session.get(FreeQuiz.class,id);
+
+        return freeQuiz;
+    }
+
+    @Override
+    public List<FreeQuiz> getFreeQuizByGrade(String grade){
+        // get current session
+        Session session = sessionFactory.getCurrentSession();
+
+        //get Grade
+        Query<FreeQuiz> freeQuizQuery = session.createQuery("from FreeQuiz where grade=:grade",FreeQuiz.class);
+        freeQuizQuery.setParameter("grade",grade);
+        List<FreeQuiz> freeQuizList = freeQuizQuery.getResultList();
+
+        return freeQuizList;
+    }
 
 }
