12283a45e48fd8cdb21aeacdf1c3920d3c3bd3d6
Kalana <kalana.madusanka26765@gmail.com>
2021-09-12 20:45:00 +0530

    Update SpecialQuizService.java

--- src/main/java/net/enLearn/service/SpecialQuizService.java
+++ src/main/java/net/enLearn/service/SpecialQuizService.java
@@ -1,8 +1,18 @@
 package net.enLearn.service;
 
+import net.enLearn.entity.SpecialQuiz;
+
+import java.util.List;
+
 /**
  * Created by Osini Kithma
  * 8/26/21
  */
 public interface SpecialQuizService {
+
+    List<SpecialQuiz> getSpecialQuizList();
+    void saveOrUpdateSpecialQuiz(SpecialQuiz specialQuiz);
+    void deleteSpecialQuiz(int id);
+    List<SpecialQuiz> getSpecialQuizByTeacherId(int id);
+    SpecialQuiz getSpecialQuizByVideoId(int id);
 }
