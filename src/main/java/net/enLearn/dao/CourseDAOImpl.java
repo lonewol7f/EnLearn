0bc969d011b23ac18d5f70868158d5895c6c9b35
Kalana <kalana.madusanka26765@gmail.com>
2021-09-12 12:40:19 +0530

    Update CourseDAOImpl.java

--- src/main/java/net/enLearn/dao/CourseDAOImpl.java
+++ src/main/java/net/enLearn/dao/CourseDAOImpl.java
@@ -1,41 +1,33 @@
 package net.enLearn.dao;
 
 import net.enLearn.entity.Course;
+import net.enLearn.entity.Teacher;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class CourseDAOImpl implements CourseDAO{
+public class CourseDAOImpl implements CourseDAO {
 
     @Autowired
     private SessionFactory sessionFactory;
 
-    //    @Override
-    //    public List<Course> getCourseListByTeacherId(int id) {
-    //        Session session = sessionFactory.getCurrentSession();
-    //        Teacher teacher = session.get(Teacher.class, id);
-    //        List<Course> courses = teacher.getCourseList();
-    //        return courses;
-    //    }
-
     @Override
-    public List<Course> getCourseListByTeacherId() {
+    public List<Course> getCourseListByTeacherId(int id) {
         Session session = sessionFactory.getCurrentSession();
-        Query<Course> courseQuery = session.createQuery("from Course", Course.class);
-        List<Course> courses = courseQuery.getResultList();
+
+        Teacher teacher = session.get(Teacher.class, id);
+        List<Course> courses = teacher.getCourseList();
         return courses;
     }
+
 
     @Override
     public void saveOrUpdate(Course course) {
         Session session = sessionFactory.getCurrentSession();
-        //        Teacher teacher = course.getTeacher();
-        //        course.setTeacher(teacher);
         session.saveOrUpdate(course);
     }
 
@@ -50,7 +42,7 @@ public class CourseDAOImpl implements CourseDAO{
     public void deleteCourse(int id) {
         Session session = sessionFactory.getCurrentSession();
         Course course = session.get(Course.class, id);
-        //        course.getTeacher().getCourseList().remove(course);
+        course.getTeacher().getCourseList().remove(course);
         session.delete(course);
 
     }
