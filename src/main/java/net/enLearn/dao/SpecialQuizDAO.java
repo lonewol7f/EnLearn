1fe14cc764d96a300302ae4e190fc200313bf898
Kalana <kalana.madusanka26765@gmail.com>
2021-09-12 20:44:40 +0530

    Update SpecialQuizDAO.java

--- src/main/java/net/enLearn/dao/SpecialQuizDAO.java
+++ src/main/java/net/enLearn/dao/SpecialQuizDAO.java
@@ -1,8 +1,19 @@
 package net.enLearn.dao;
 
+import net.enLearn.entity.SpecialQuiz;
+
+import java.util.List;
+
 /**
  * Created by Osini Kithma
  * 8/25/21
  */
 public interface SpecialQuizDAO {
+
+    List<SpecialQuiz> getSpecialQuizList();
+    void saveOrUpdateSpecialQuiz(SpecialQuiz specialQuiz);
+    void deleteSpecialQuiz(int id);
+    List<SpecialQuiz> getSpecialQuizByTeacherId(int id);
+    SpecialQuiz getSpecialQuizByVideoId(int id);
+
 }
