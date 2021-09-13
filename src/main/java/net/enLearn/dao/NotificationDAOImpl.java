3751c15c70d2e3ae1722f474ee5ca076d385ce00
Kalana <kalana.madusanka26765@gmail.com>
2021-09-13 07:37:06 +0530

    Update NotificationDAOImpl.java

--- src/main/java/net/enLearn/dao/NotificationDAOImpl.java
+++ src/main/java/net/enLearn/dao/NotificationDAOImpl.java
@@ -1,17 +1,21 @@
 package net.enLearn.dao;
 
 import net.enLearn.entity.Notification;
+import net.enLearn.entity.Teacher;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Kalana on 04/09/2021
  */
 
 @Repository
-public class NotificationDAOImpl implements NotificationDAO{
+public class NotificationDAOImpl implements NotificationDAO {
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -21,5 +25,18 @@ public class NotificationDAOImpl implements NotificationDAO{
         Session session = sessionFactory.getCurrentSession();
 
         session.save(notification);
+    }
+
+    @Override
+    public List<Notification> getNotificationByTeacherId(int teacherId) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Teacher teacher = session.get(Teacher.class, teacherId);
+
+        Query<Notification> notificationQuery = session.createQuery("from Notification where teacher= :teacher order by id DESC", Notification.class);
+        notificationQuery.setParameter("teacher", teacher);
+        List<Notification> notifications = notificationQuery.getResultList();
+
+        return notifications;
     }
 }
