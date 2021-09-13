47618a8691154fcdf5a05c575bfbeba281e70c42
Kalana <kalana.madusanka26765@gmail.com>
2021-09-13 07:37:29 +0530

    Update NotificationService.java

--- src/main/java/net/enLearn/service/NotificationService.java
+++ src/main/java/net/enLearn/service/NotificationService.java
@@ -2,6 +2,8 @@ package net.enLearn.service;
 
 import net.enLearn.entity.Notification;
 
+import java.util.List;
+
 /**
  * Created by Kalana on 04/09/2021
  */
@@ -9,4 +11,5 @@ import net.enLearn.entity.Notification;
 public interface NotificationService {
 
     void addNotification(Notification notification);
+    List<Notification> getNotificationByTeacherId(int teacherId);
 }
