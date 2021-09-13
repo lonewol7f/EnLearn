b3b5bd39799888fb4fcc9379070246881456688e
Kalana <kalana.madusanka26765@gmail.com>
2021-09-13 13:10:57 +0530

    Create ReportController.java

---
+++ src/main/java/net/enLearn/controller/ReportController.java
@@ -0,0 +1,59 @@
+package net.enLearn.controller;
+
+import net.enLearn.entity.Event;
+import net.enLearn.service.EventService;
+import net.sf.jasperreports.engine.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+
+/**
+ * Created by Kalana on 13/09/2021
+ */
+
+@Controller
+@RequestMapping("/reports")
+public class ReportController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/single-event")
+    public ResponseEntity<byte[]> generateSingleEventReport(@RequestParam("eventId") int id) throws Exception, JRException {
+
+        Event event = eventService.getEventById(id);
+
+        Resource resource = new ClassPathResource("reports/single-event.jrxml");
+        File file = resource.getFile();
+
+        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream(file));
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("topic", event.getTopic());
+        map.put("message", event.getMessage());
+        map.put("date", event.getDate());
+        map.put("time", event.getTime());
+
+        JasperPrint report = JasperFillManager.fillReport(compileReport,map, new JREmptyDataSource());
+
+        byte[] data = JasperExportManager.exportReportToPdf(report);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=event.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+
+    }
+
+}
