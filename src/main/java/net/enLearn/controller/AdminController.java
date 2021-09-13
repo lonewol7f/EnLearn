cb839f02837a87a9a3d02c46111da6d4c1e31166
Kalana <kalana.madusanka26765@gmail.com>
2021-09-13 09:46:49 +0530

    Update AdminController.java

--- src/main/java/net/enLearn/controller/AdminController.java
+++ src/main/java/net/enLearn/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -69,10 +70,11 @@ public class AdminController {
     }
 
     @GetMapping("/remove-used-coupons")
-    public String removeUsedCoupon() {
+    public String removeUsedCoupon(RedirectAttributes redirectAttributes) {
         redeemCodeService.deleteUsedCodes();
 
-        return "redirect:/test-lang";  // TODO: edit this redirect to correct mapping
+        redirectAttributes.addAttribute("remove-used");
+        return "redirect:/admins/codes";  // TODO: edit this redirect to correct mapping
     }
 
     @GetMapping("/codes")
