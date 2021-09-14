7950187b34abd53e86ad2f779800ed0358f0a8a5
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-09-14 12:46:16 +0530

    Update UserServiceImpl.java

--- src/main/java/net/enLearn/service/UserServiceImpl.java
+++ src/main/java/net/enLearn/service/UserServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+/**
+ * Created by Kalana on 30/07/2021
+ */
 
 @Service
 public class UserServiceImpl implements UserService {
