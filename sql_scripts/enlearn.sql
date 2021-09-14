9dc36a64d73dc774e55418ad0ad84564784d3b5a
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-09-14 07:57:42 +0530

    Update enlearn.sql

--- sql_scripts/enlearn.sql
+++ sql_scripts/enlearn.sql
@@ -73,13 +73,6 @@ CREATE TABLE `advertiser`
 (
     `id`           INT NOT NULL,
     `company_name` VARCHAR(50),
-    `email`      VARCHAR(100),
-    `contact_no`     CHAR(10),
-    `password`   VARCHAR(60) NOT NULL,
-    `image`       BLOB,
-
-
-
     CONSTRAINT `pk_advertiser` PRIMARY KEY (`id`),
     CONSTRAINT `fk_advertiser` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB
