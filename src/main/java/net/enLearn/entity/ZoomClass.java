40364c8e59cdb3aa09e9ddda02e33c3e54e84c66
deadp00l <hasinthadhanushka@gmail.com>
2021-09-20 03:23:23 +0530

    some zoom class parts mod

--- src/main/java/net/enLearn/entity/ZoomClass.java
+++ src/main/java/net/enLearn/entity/ZoomClass.java
@@ -1,10 +1,33 @@
 package net.enLearn.entity;
 
+import javax.persistence.*;
+
 public class ZoomClass {
 
-//    @Entity
-//    @Table(name = "zoom_class")
-//    public class ZoomClass {
-//
-//    }
+    @Entity
+    @Table(name = "zoom_class")
+    public class ZoomClass {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private int id;
+
+        @Column(name = "title")
+        private String title;
+
+        @Column(name = "description")
+        private String description;
+
+        @Column(name = "date")
+        private String date;
+
+
+        @Column(name = "time")
+        private String time;
+
+        @Column(name = "zoom_link")
+        private String zoom_link;
+
+    }
 }
