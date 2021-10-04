d04d7df90b3e92dabe1fc633780fe6f2648739a3
Kalana <kalana.madusanka26765@gmail.com>
2021-10-04 12:22:38 +0530

    Update Student.java

--- src/main/java/net/enLearn/entity/Student.java
+++ src/main/java/net/enLearn/entity/Student.java
@@ -1,6 +1,8 @@
 package net.enLearn.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,18 +14,28 @@ import java.util.List;
 @Entity
 @Table(name = "student")
 @PrimaryKeyJoinColumn(name = "id")
-public class Student extends User{
+public class Student extends User {
 
     @Column(name = "grade")
     @JsonBackReference
     private int grade;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinTable(name = "special_quiz_student",
             joinColumns = @JoinColumn(name = "student_id"),
             inverseJoinColumns = @JoinColumn(name = "quiz_id"))
+    @Fetch(value = FetchMode.SUBSELECT)
     @JsonBackReference
     private List<SpecialQuiz> specialQuizList;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    @Fetch(value = FetchMode.SUBSELECT)
+    @JsonBackReference
+    private List<Course> courseList;
 
     public Student() {
     }
@@ -42,5 +54,13 @@ public class Student extends User{
 
     public void setSpecialQuizList(List<SpecialQuiz> specialQuizList) {
         this.specialQuizList = specialQuizList;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
     }
 }
