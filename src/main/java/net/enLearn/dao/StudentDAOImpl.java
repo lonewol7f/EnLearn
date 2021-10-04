020b4473fba2eec35d7f9f7f43d66ed82874dfd4
Kalana <kalana.madusanka26765@gmail.com>
2021-10-04 12:22:27 +0530

    Update StudentDAOImpl.java

--- src/main/java/net/enLearn/dao/StudentDAOImpl.java
+++ src/main/java/net/enLearn/dao/StudentDAOImpl.java
@@ -1,11 +1,14 @@
 package net.enLearn.dao;
 
+import net.enLearn.entity.Course;
 import net.enLearn.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Kalana on 10/09/2021
@@ -27,5 +30,24 @@ public class StudentDAOImpl implements StudentDAO{
                 "VALUES (:studentId, 1)");
         query.setParameter("studentId", studentId);
         query.executeUpdate();
+    }
+
+    @Override
+    public List<Course> getEnrolledCoursesByStudentId(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Student student = session.get(Student.class, id);
+
+        List<Course> courseList = student.getCourseList();
+
+        return courseList;
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Student student = session.get(Student.class, id);
+
+        return student;
     }
 }
